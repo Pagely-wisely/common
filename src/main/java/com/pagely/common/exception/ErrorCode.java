937be402beab8221bb9acafc8221617f3752966c
soo <dlgustn96@gmail.com>
2026-04-26 00:03:14 +0900

    feat: BusinessException 및 공통 에러코드 정의

--- src/main/java/com/pagely/common/exception/ErrorCode.java
+++ src/main/java/com/pagely/common/exception/ErrorCode.java
@@ -1,4 +1,15 @@
 package com.pagely.common.exception;
 
+import org.springframework.http.HttpStatus;
+
 public interface ErrorCode {
+
+    // ErrorCode Enum Name (예시: BAD_REQUEST, USER_NOT_FOUND)
+    String getCode();
+
+    // 사용자에게 노출할 메시지
+    String getMessage();
+
+    // HTTP 상태 코드
+    HttpStatus getHttpStatus();
 }
